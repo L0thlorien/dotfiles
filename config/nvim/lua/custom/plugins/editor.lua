f1b5616ec0a59f7e8e458348a2b7b92cf46265db
Aidar Siraev <aidarsiraev12@gmail.com>
2026-06-18 18:54:51 +0300

    chore: pseudotile deprecated parameter removed

--- config/nvim/lua/custom/plugins/editor.lua
+++ config/nvim/lua/custom/plugins/editor.lua
@@ -57,4 +57,13 @@ return {
       },
     },
   },
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    ft = { 'markdown' },
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'nvim-tree/nvim-web-devicons',
+    },
+    opts = {},
+  },
 }
